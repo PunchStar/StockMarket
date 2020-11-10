99dddd2a7a315d193edecb15b83579a4a23a9886
FullStackDevel <superpunch@gmail.com>
2020-11-10 04:11:18 -0800

    add

--- config/express.js
+++ config/express.js
@@ -27,16 +27,13 @@ module.exports = () => {
     // routes
     require('../routes/index')(app);
     require('../routes/User.routes')(app);
-    if(process.env.NODE_ENV === "development")
-    {
+    // if(process.env.NODE_ENV === "development")
+    // {
         // CORS rquests
         app.use("/", (req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-            res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-            );
+            res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
             next();
             // res.header("Access-Control-Allow-Origin", "https://stock-market-good.herokuapp.com/");
             // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept, Authorization");
@@ -46,6 +43,6 @@ module.exports = () => {
             res.sendFile(path.join(__dirname, '../client', 'demo', 'index.html'));
             // console.log
         })
-    } 
+    // } 
     return app;
 };
