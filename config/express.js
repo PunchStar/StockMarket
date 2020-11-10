59ec5ad6441b816f8cc9c2d818a07123ef989c5d
FullStackDevel <superpunch@gmail.com>
2020-11-10 03:48:58 -0800

    update

--- config/express.js
+++ config/express.js
@@ -31,9 +31,16 @@ module.exports = () => {
     {
         // CORS rquests
         app.use("/", (req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "https://stock-market-good.herokuapp.com/");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept, Authorization");
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+            res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+            );
             next();
+            // res.header("Access-Control-Allow-Origin", "https://stock-market-good.herokuapp.com/");
+            // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept, Authorization");
+            // next();
         });
         app.get('*',(req,res)=>{
             res.sendFile(path.join(__dirname, '../client', 'demo', 'index.html'));
